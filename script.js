8ea546216f960dd6be59b772733d4c0670602922
sohelhussain <sohelhussaing@gmail.com>
2024-02-06 21:24:52 -0800

    feat:-creating a front hero image

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 const scroller = () => {
-    const lenis = new Lenis()
+  const lenis = new Lenis();
 
-lenis.on('scroll', ScrollTrigger.update)
+  lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
 
-gsap.ticker.lagSmoothing(0)
-}
+  gsap.ticker.lagSmoothing(0);
+};
 scroller();
 const loder = () => {
-    gsap.to("#loder",{
-        y:"-100%",
-        duration:.8,
-        display:"none",
-        ease: "power4.out",
-    })
-}
+  let tl = gsap.timeline();
+  tl.to("#loder", {
+    top: "-100%",
+    duration: 0.8,
+    display: "none",
+    ease: "power4.out",
+  });
+  // tl.to(".front-img", {
+  //   //  clipPath: `polygon(0 100%, 100% 100%, 100% 0, 0 0)`,
+  //   height: "90%",
+  //   duration: 3,
+  // });
+  tl.from("nav", {
+    y: "-100",
+    // delay: 0.8,
+  });
+};
 loder();
-gsap.from("nav",{
-    y:"100",
-})
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#conatiner-first>.part-2",
+    scroller: "body",
+    start: "0% -20%",
+    end: "100% -200%",
+    markers: true,
+    scrub: 1,
+    pin: true,
+  },
+});
+tl2.to(".front-img", {
+  width: "35%",
+  height: "70vh",
+});
